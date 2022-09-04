92934953d70428a0c0cc9cffe4297231dd6fb58d
Zaid alshibi <zaidealshibi@gmail.com>
2022-09-04 16:19:58 +0300

    added cors for cross origin

--- server.js
+++ server.js
@@ -1,24 +1,43 @@
 'use strict';
 
+
+
+// Cors for cross-origin allowance
+const cors = require( "cors" );
+app.use( cors() );
+
+
+
+// Express
 const express = require( 'express' );
 const app = express();
+app.use( express.json() );
 
+
+
+// Check for alive 
 app.get( '/', ( req, res ) => {
     res.status( 200 ).send( 'Hello World' );
 } );
 
+
+// Person endpoint
 app.post( '/person', ( req, res ) => {
     const name = req.query.name;
     const age = req.query.age;
-    const gender = req.query.gender
+    const gender = req.query.gender;
     const newAge = parseInt( age ) + 5;
     res.status( 200 ).send( `${newAge}` );
 } );
 
+// Port listen 
 function start ( port ) {
     app.listen( port, () => console.log( `server up a wake` ) );
 }
 
+
+
+// Export
 module.exports = {
     app: app,
     start: start
